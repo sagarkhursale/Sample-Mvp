9093b6f95b619991b31e8b86223342d5ba5a8829
sagar khursale <sagar.khursale@webmatrixtech.com>
2018-09-23 03:33:50 +0530

    complete marker interface

--- app/src/main/java/com/sagar/samplemvp/login/LoginActivityMvp.java
+++ app/src/main/java/com/sagar/samplemvp/login/LoginActivityMvp.java
@@ -4,17 +4,37 @@ package com.sagar.samplemvp.login;
 
 public interface LoginActivityMvp {
 
-    interface View{
+
+    interface View {
+
+        String getFirstName();
+        String getLastName();
+
+        void showUserNotAvailable();
+        void showInputError();
+        void showUserSavedMessage();
+
+        void setFirstName(String firstName);
+        void setLastName(String lastName);
 
     }
 
 
-    interface Presenter{
+    interface Presenter {
 
+        void setView(LoginActivityMvp.View view);
+
+        void loginButtonClicked();
+
+        void getCurrentUser();
     }
 
 
-    interface Model{
+    interface Model {
+
+        void creatUser(String fname,String lname);
+
+        User getUser();
 
     }
 
