4d361b0b5ca8be7936bcea9483e164794938ffc0
sagar khursale <sagar.khursale@webmatrixtech.com>
2018-09-23 02:34:37 +0530

    login_activity.xml

--- app/src/main/java/com/sagar/samplemvp/LoginActivity.java
+++ app/src/main/java/com/sagar/samplemvp/LoginActivity.java
@@ -3,11 +3,16 @@ package com.sagar.samplemvp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
 public class LoginActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+
+
+    // END
 }
