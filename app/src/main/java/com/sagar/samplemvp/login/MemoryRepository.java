8cd26f84eb0e2f4bbb066d7e829fb4ddfa36ecdc
sagar khursale <sagar.khursale@webmatrixtech.com>
2018-09-23 03:16:23 +0530

    implement Login Repo in MemoryRepo

---
+++ app/src/main/java/com/sagar/samplemvp/login/MemoryRepository.java
@@ -0,0 +1,20 @@
+package com.sagar.samplemvp.login;
+
+
+public class MemoryRepository implements LoginRepository {
+
+
+    @Override
+    public User getUser() {
+        return null;
+    }
+
+
+    @Override
+    public void saveUser() {
+
+    }
+
+
+    // END
+}
