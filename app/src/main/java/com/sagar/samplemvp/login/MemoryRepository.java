81f4f10ccc9008f14eeacee1a5dff8e02466274c
sagar khursale <sagar.khursale@webmatrixtech.com>
2018-09-23 05:14:25 +0530

    update MemoryRepository

--- app/src/main/java/com/sagar/samplemvp/login/MemoryRepository.java
+++ app/src/main/java/com/sagar/samplemvp/login/MemoryRepository.java
@@ -1,18 +1,34 @@
 package com.sagar.samplemvp.login;
 
 
+
 public class MemoryRepository implements LoginRepository {
+
+    private User user;
+
 
 
     @Override
     public User getUser() {
-        return null;
+        if (user == null) {
+            User user = new User("Fox", "Mulder");
+            user.setId(0);
+            return user;
+        } else {
+            return user;
+        }
     }
 
 
-    @Override
-    public void saveUser() {
 
+    @Override
+    public void saveUser(User user) {
+
+        if (user == null) {
+            user = getUser();
+
+        }
+        this.user = user;
     }
 
 
