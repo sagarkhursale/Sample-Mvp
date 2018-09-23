1c58980ef0ca07fda2cefced39a76ba091c9a36a
sagar khursale <sagar.khursale@webmatrixtech.com>
2018-09-23 05:30:06 +0530

    add login module dependencies

--- app/src/main/java/com/sagar/samplemvp/login/LoginModule.java
+++ app/src/main/java/com/sagar/samplemvp/login/LoginModule.java
@@ -1,10 +1,29 @@
 package com.sagar.samplemvp.login;
 
 import dagger.Module;
+import dagger.Provides;
 
 
 @Module
 public class LoginModule {
+
+
+    @Provides
+    public LoginActivityMvp.Presenter provideLoginActivityPresenter(LoginActivityMvp.Model model) {
+        return new LoginActivityPresenter(model);
+    }
+
+
+    @Provides
+    public LoginActivityMvp.Model provideLoginActivityModel(LoginRepository repository) {
+        return new LoginModel(repository);
+    }
+
+
+    @Provides
+    public LoginRepository provideLoginRepository() {
+        return new MemoryRepository();
+    }
 
 
     // END
--- app/src/main/java/com/sagar/samplemvp/root/ApplicationComponent.java
+++ app/src/main/java/com/sagar/samplemvp/root/ApplicationComponent.java
@@ -1,6 +1,7 @@
 package com.sagar.samplemvp.root;
 
 import com.sagar.samplemvp.login.LoginActivity;
+import com.sagar.samplemvp.login.LoginModule;
 
 import javax.inject.Singleton;
 import dagger.Component;
@@ -8,7 +9,7 @@ import dagger.Component;
 
 
 @Singleton
-@Component(modules = {ApplicationModule.class})
+@Component(modules = {ApplicationModule.class, LoginModule.class})
 public interface ApplicationComponent {
 
     void inject(LoginActivity target);
